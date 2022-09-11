589a72dd8d78ce1e9ae2874b0f81fd7aae5346e9
Danilo Borges <borges_santos89@hotmail.com>
2022-09-11 17:36:26 -0300

    adiciona hello.asm

---
+++ assembly/hello.asm
@@ -0,0 +1,14 @@
+global _start
+
+section .data
+message: db "hello world!", 10
+
+section .text
+_start:
+	mov rax, 1 		;o número de chamada de sistema deve ser armazenado em rax
+				;0 = stdin, 1 = stdout, 2 = stderr
+
+	mov rdi, 1		;argumento #1 em rdi: onde escrever (descritor)?
+	mov rsi, message	;argumetno #2 em rsi: onde começa a string?
+	mov rdx, 14		;argumento #3 em rdx: quantos bytes devem ser escritos?
+	syscall			; chamada de sistema
