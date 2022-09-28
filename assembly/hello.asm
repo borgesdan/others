d55991f63a1e1c6ccd84ea2c169cfc7a6154eafc
Danilo Borges <borges_santos89@hotmail.com>
2022-09-28 20:06:04 -0300

    altera comentários

--- assembly/hello.asm
+++ assembly/hello.asm
@@ -5,14 +5,14 @@ message: db "hello world!", 10
 
 section .text
 _start:
-	mov rax, 1 		;o número de chamada de sistema deve ser armazenado em rax
-				;0 = stdin, 1 = stdout, 2 = stderr
+	mov rax, 1 			;o número de chamada de sistema deve ser armazenado em rax
+						;0 = stdin, 1 = stdout, 2 = stderr
 
-	mov rdi, 1		;argumento #1 em rdi: onde escrever (descritor)?
+	mov rdi, 1			;argumento #1 em rdi: onde escrever (descritor)?
 	mov rsi, message	;argumento #2 em rsi: onde começa a string?
-	mov rdx, 14		;argumento #3 em rdx: quantos bytes devem ser escritos?
+	mov rdx, 14			;argumento #3 em rdx: quantos bytes devem ser escritos?
 	syscall
 
-	mov rax, 60		;chamada de saída
+	mov rax, 60			;chamada de saída
 	xor rdi, rdi		;valor de retorno do programa, 0
 	syscall
